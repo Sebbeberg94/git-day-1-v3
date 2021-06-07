16c58ccb318fa7a28e2f52e421549b8af3f6c928
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-07 13:19:13 +0200

    New feature

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(returnInt(5));
+        System.out.println(returnIntUp(5));
+        System.out.println(returnIntDown(5));
     }
-    public static int returnInt(int x) {
+    public static int returnIntUp(int x) {
         return x + 1;
+    }
+    public static int returnIntDown(int x) {
+        return x - 1;
     }
 }
