d9f2e7e615dbb4fed1e073973ad951bd50c901bd
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-07 15:02:46 +0200

    Sista ändringar gjorda

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(returnInt(5));
         System.out.println(returnMulti(2, 5));
         System.out.println(returnIntUp(5));
         System.out.println(returnIntDown(5));
     }
-    public static int returnInt(int x) {
-        return x + 1;
-    }
     public static int returnMulti(int x, int y) {
         return x * y;
+    }
+    public static int returnIntUp(int x) {
+        return x + 1;
     }
     public static int returnIntDown(int x) {
         return x - 1;
