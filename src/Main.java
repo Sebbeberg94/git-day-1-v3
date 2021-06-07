7c2f2dc1dc59a6174c316e02a50352e30a69ed47
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-07 14:51:10 +0200

    haha

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(returnInt(5));
-        System.out.println(returnIntMult(3, 4));
+
     }
     public static int returnInt(int x) {
         return x + 1;
     }
-    public static int returnIntMult(int x, int y) {
+    public static int returnMulti(int x, int y) {
         return x * y;
     }
 }
