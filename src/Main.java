d3397bfc22913bbf796b2e7322c5d1bce94a6e54
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-07 12:56:45 +0200

    new files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(returnInt(5));
+    }
+    public static int returnInt(int x) {
+        return x + 1;
+    }
+}
